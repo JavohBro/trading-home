317c74807a260d208b9cca456526fd280349db94
javohbro <salohiddinovj73@gmail.com>
2024-04-30 18:18:31 +0500

    last configuration

--- link/models.py
+++ link/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    username = models.CharField(blank=True, null=True, max_length=150)
+    name = models.CharField(max_length=150)
+    user_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
